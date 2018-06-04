2b0d742c70e0a8ece7986e931b2d9dcc03127315
ruffle1986 <ftamas.mail@gmail.com>
2018-06-04 21:59:21 +0200

    feat(messages): add components

--- src/components/message.js
+++ src/components/message.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-const Message = () => {
-  return 'Hello message';
+const Message = (props) => {
+  return (
+    <div
+      data-id={ props.id }
+      className={ classNames('message', {
+        [`message--${props.type}`]: true,
+      }) }
+    >
+      { props.body }
+    </div>
+  );
 };
 
 Message.displayName = 'Message';
+
+const propTypes = {
+  id: PropTypes.number.isRequired,
+  type: PropTypes.string.isRequired,
+  body: PropTypes.string.isRequired,
+};
+
+export const messageShape = PropTypes.shape(propTypes);
+
+Message.propTypes = propTypes;
 
 export default Message;
--- src/components/messages.js
+++ src/components/messages.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Message, { messageShape } from './message';
 
-const Messages = () => {
-  return 'Hello messages';
+const Messages = (props) => {
+  return (
+    <div className="messages">
+      {
+        props.messages.map((message) => (
+          <Message { ...message } key={ message.id } />
+        ))
+      }
+    </div>
+  );
 };
 
 Messages.displayName = 'Messages';
+
+Messages.propTypes = {
+  messages: PropTypes.arrayOf(messageShape),
+};
+
+Messages.defaultProps = {
+  messages: []
+};
 
 export default Messages;
