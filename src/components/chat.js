cd4aaf189d758a648e7ca4ce1ed36f5848040ee3
ruffle1986 <ftamas.mail@gmail.com>
2018-06-04 22:49:04 +0200

    feat: implement /think command

--- src/components/chat.js
+++ src/components/chat.js
@@ -6,16 +6,62 @@ import { isCommand, parseCommand } from '../utils/command';
 
 class Chat extends React.Component {
   static displayName = 'Chat'
+
+  state = {
+    messages: []
+  }
+
+  setName = () => {
+    // to be implemented
+  }
+
+  addMessage = (body, type = 'user') => {
+    this.setState((state) => ({
+      messages: state.messages.concat({
+        id: Date.now(),
+        body,
+        type
+      })
+    }))
+  }
+
+  removeLastMessage = () => {
+    // to be implemented
+  }
+
+  handleCommand(command, args) {
+
+    switch (command) {
+      case 'nick': {
+        this.setName();
+        break;
+      }
+      case 'think': {
+        if (!args) {
+          alert('Invalid argument.')
+        }
+        this.addMessage(args[0], 'thought');
+        break;
+      }
+      case 'oops': {
+        this.removeLastMessage();
+        break;
+      }
+      default:
+        break;
+    }
+
+  }
   render() {
     return (
       <div className="chat">
         <PartnerInfo />
-        <Messages messages={ this.props.messages } />
+        <Messages messages={ this.state.messages } />
         <Input
           onSend={ (message) => {
             if (isCommand(message)) {
-              const command = parseCommand(message);
-              console.log(command);
+              const { command, args } = parseCommand(message);
+              this.handleCommand(command, args);
             }
           } }
         />
--- src/components/chat.test.js
+++ src/components/chat.test.js
@@ -5,3 +5,18 @@ test('it should render properly', () => {
   const wrapper = shallow(<Chat />);
   expect(wrapper).toMatchSnapshot();
 });
+
+test('it should add a thought', () => {
+  const wrapper = mount(<Chat />);
+  const input = wrapper.find('[data-test="input"]');
+  const button = wrapper.find('[data-test="button"]');
+  const arg = 'Oops I did it again';
+
+  expect(wrapper.find('.message--thought')).toHaveLength(0);
+
+  input.simulate('change', { target: { value: `/think "${arg}"` } });
+  button.simulate('click');
+
+  expect(wrapper.find('.message--thought')).toHaveLength(1);
+  expect(wrapper.find('.message--thought').text()).toBe(arg);
+});
