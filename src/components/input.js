98f74c605578f846fd00dff33323777617f5bd0f
ruffle1986 <ftamas.mail@gmail.com>
2018-06-04 21:24:49 +0200

    feat: add input structure, handle message sending

--- src/components/input.js
+++ src/components/input.js
@@ -1,8 +1,71 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Input = () => {
-  return 'Hello input';
-};
+const KEY_ENTER = 13;
+
+class Input extends React.Component {
+
+  static propTypes = {
+    onSend: PropTypes.func,
+    onType: PropTypes.func,
+  }
+
+  static defaultProps = {
+    onSend: () => {},
+    onType: () => {},
+  }
+
+  state = {
+    value: '',
+  }
+
+  empty = () => {
+    this.setState({
+      value: ''
+    });
+  }
+
+  send = (value) => {
+    this.props.onSend(value);
+    this.empty();
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    });
+  }
+
+  handleKeyUp = (e) => {
+    if (e.keyCode === KEY_ENTER) {
+      this.send(e.target.value);
+    } else {
+      this.props.onType(e.target.value);
+    }
+  }
+
+  handleButtonClick = () => {
+    this.send(this.state.value);
+  }
+
+  render() {
+    return (
+      <div className="input">
+        <input
+          data-test="input"
+          type="text"
+          value={ this.state.value }
+          onChange={ this.handleChange }
+          onKeyUp={ this.handleKeyUp }
+        />
+        <button
+          data-test="button"
+          onClick={ this.handleButtonClick }
+        />
+      </div>
+    );
+  }
+}
 
 Input.displayName = 'Input';
 
