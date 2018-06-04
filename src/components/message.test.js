065ed5e348737e7297a89ab98fe9738accfb09d8
ruffle1986 <ftamas.mail@gmail.com>
2018-06-04 22:00:08 +0200

    test(messages): add tests and snapshots.

--- src/components/message.test.js
+++ src/components/message.test.js
@@ -2,6 +2,12 @@ import React from 'react';
 import Message from './message';
 
 test('it should render properly', () => {
-  const wrapper = shallow(<Message />);
+  const wrapper = shallow(
+    <Message
+      id={ 1 }
+      type="user"
+      body="hello world!"
+    />
+  );
   expect(wrapper).toMatchSnapshot();
 });
--- src/components/messages.test.js
+++ src/components/messages.test.js
@@ -2,6 +2,21 @@ import React from 'react';
 import Messages from './messages';
 
 test('it should render properly', () => {
-  const wrapper = shallow(<Messages />);
+  const wrapper = shallow(
+    <Messages
+      messages={[
+        {
+          id: 1,
+          type: 'user',
+          body: 'foo'
+        },
+        {
+          id: 2,
+          type: 'user',
+          body: 'bar'
+        }
+      ]}
+    />
+  );
   expect(wrapper).toMatchSnapshot();
 });
